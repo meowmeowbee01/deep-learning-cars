3a457b6b63563e0c4f8189ecacc760203c113903
Arno <pamerichten@gmail.com>
2023-10-13 18:55:39 +0200

    tims experiment opruimen

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-print("hello world")
-print("hello world")
+import arcade
+
+arcade.open_window(600, 600, "Drawing Example")
+
+arcade.set_background_color([255,127,127])
+
+arcade.start_render()
+
+arcade.draw_lrtb_rectangle_filled(0, 300, 300, 0, [0,0,0])
+
+arcade.finish_render()
+
+arcade.run()
